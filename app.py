d81ad5582a748aa0b5dfcf6b27d86c1107e42b1b
Chittah <symohgakuru@gmail.com>
2026-09-10 12:57:28 +0300

    Fix database circular import

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from extensions import db
 
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-db = SQLAlchemy(app)
+db.init_app(app)
 
 migrate = Migrate(app, db)
 
